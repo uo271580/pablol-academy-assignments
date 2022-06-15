bd3052917f9c54f7a7a90f48a1b893993261514f
Pablo Lopez <pablol@empathy.co>
2022-06-15 11:17:29 +0200

    Create new project and install .jar

---
+++ mavenFirstSteps/src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public void minus(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public void multiply(int a, int b) {
+        System.out.println(a * b);
+    }
+
+    public void divide(int a, int b){
+        System.out.println(a / b);
+    }
+}
