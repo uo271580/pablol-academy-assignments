1c0addf9195053b98e72149b21b682fe539999fa
Pablo Lopez <pablol@empathy.co>
2022-06-20 09:41:06 +0200

    Reordering

--- DemoAcademy1/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Crawler crawler = new Crawler();
-        crawler.crawl("https://sede.agenciatributaria.gob.es/", 20);
-    }
-}
