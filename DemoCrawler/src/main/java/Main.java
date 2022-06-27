060e232c7c6cb76b4b2e8c4112082203af7b3553
Pablo Lopez <pablo268la@gmail.com>
2022-06-27 10:47:43 +0200

    Deleting DemoCrawler from repo
    Now there is a specific repo for it

--- DemoCrawler/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Crawler crawler = new Crawler();
-        //crawler.crawl("https://www.google.com/", 20);
-        crawler.crawl("https://www.infosubvenciones.es/bdnstrans/GE/es/index", 20);
-    }
-}
